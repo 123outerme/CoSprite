5c705f97ff375b0ea6230e7a6511cbc5b096f547
Stephen Policelli <123outerme@outlook.com>
2018-06-25 15:04:00 -0400

    Fixed issue with texts, fixed main.c

--- main.c
+++ main.c
@@ -3,22 +3,22 @@
 #include "crankGraphics.h"
 #include "crankUtility.h"
 
-void loadSprite(cSprite* sprite, char* filePath, SDL_Rect rect, SDL_Rect clipRect, double scale, SDL_RendererFlip flip, double degrees, void* subclass, int priority);
+void loadSprite(cSprite* sprite, char* filePath, SDL_Rect rect, SDL_Rect clipRect, double scale, SDL_RendererFlip flip, double degrees, bool fixed, void* subclass, int priority);
 
 int main(int argc, char* argv[])
 {
     int code = initCrank("cb.bmp", "Crank Test", 960, 480, "Px437_ITT_BIOS_X.ttf", 24);
-    cSprite sprites[2];
-    cText txts[1];
-    loadSprite(&sprites[0], "cb.bmp", (SDL_Rect) {.x = 50, .y = 50, .w = 150, .h = 120}, (SDL_Rect) {0, 0, 150, 120}, 1.0, SDL_FLIP_NONE, 0, NULL, 4);
-    loadSprite(&sprites[1], "cb.bmp", (SDL_Rect) {.x = 0, .y = 0, .w = 150, .h = 120}, (SDL_Rect) {0, 0, 150, 120}, 1.0, SDL_FLIP_NONE, 0, NULL, 5);
-    initCText(&txts[0], "Hello world!", (SDL_Rect) {150, 150, 300, 300}, (SDL_Color) {0, 0, 0, 0xFF}, (SDL_Color) {0xFF, 0, 0, 0x00}, 1);
+    cSprite lowerSprite, upperSprite;
+    cText txt;
+    loadSprite(&lowerSprite, "cb.bmp", (SDL_Rect) {50, 50, 150, 120}, (SDL_Rect) {0, 0, 150, 120}, 1.0, SDL_FLIP_NONE, 0, false, NULL, 4);
+    loadSprite(&upperSprite, "cb.bmp", (SDL_Rect) {0, 0, 150, 120}, (SDL_Rect) {0, 0, 150, 120}, 1.0, SDL_FLIP_NONE, 0, false, NULL, 5);
+    initCText(&txt, "Hello world!", (SDL_Rect) {150, 150, 300, 300}, (SDL_Color) {0, 0, 0, 0xFF}, (SDL_Color) {0xFF, 0, 0, 0x00}, false, 1);
     c2DModel model;
-    initC2DModel(&model, sprites, 2, 0, 0, 150, 150, 1.0, SDL_FLIP_NONE, 0.0, NULL, 5);
+    initC2DModel(&model, (cSprite[2]) {lowerSprite, upperSprite}, 2, 0, 0, 150, 150, 1.0, SDL_FLIP_NONE, 0.0, false, NULL, 5);
     cCamera camera;
-    initCCamera(&camera, (SDL_Rect) {0, 0, windowW / 48, windowH / 48}, 1.0);
+    initCCamera(&camera, (SDL_Rect) {0, 0, windowW / 48, windowH / 48}, 1.0, 0.0);
     cScene scene;
-    initCScene(&scene, (SDL_Color) {0xFF, 0xFF, 0xFF, 0xFF}, &camera, sprites, 0, &model, 1, (cResource*) NULL, 0, txts, 1);
+    initCScene(&scene, (SDL_Color) {0xFF, 0xFF, 0xFF, 0xFF}, &camera, (cSprite*[2]) {&lowerSprite, &upperSprite}, 0, (c2DModel*[1]) {&model}, 1, (cResource**) NULL, 0, (cText*[1]) {&txt}, 1);
     int key;
     while(key != -1)
     {
@@ -45,9 +45,9 @@ int main(int argc, char* argv[])
     return code;
 }
 
-void loadSprite(cSprite* sprite, char* filePath, SDL_Rect rect, SDL_Rect clipRect, double scale, SDL_RendererFlip flip, double degrees, void* subclass, int priority)
+void loadSprite(cSprite* sprite, char* filePath, SDL_Rect rect, SDL_Rect clipRect, double scale, SDL_RendererFlip flip, double degrees, bool fixed, void* subclass, int priority)
 {
     SDL_Texture* tempTexture;
     loadIMG(filePath, &tempTexture);
-    initCSprite(sprite, tempTexture, 1, rect, clipRect, scale, flip, degrees, subclass, priority);
+    initCSprite(sprite, tempTexture, 1, rect, clipRect, scale, flip, degrees, fixed, subclass, priority);
 }
